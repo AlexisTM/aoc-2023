fc3f272d7c6f69dc2678f6165ed48b2e94f6d218
Alexis Paques <paa1ti@bosch.com>
2023-12-05 13:09:07 +0100

    Add day 5p2

--- days/05.rs
+++ days/05.rs
@@ -1,7 +1,7 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Map {
-    destination: i64,
     source: i64,
+    destination: i64,
     length: i64,
 }
 
@@ -18,8 +18,8 @@ impl Map {
             println!("Incorrect data... {:?}", input);
         }
         Self {
-            destination: input[0].parse().unwrap(),
             source: input[1].parse().unwrap(),
+            destination: input[0].parse().unwrap(),
             length: input[2].parse().unwrap(),
         }
     }
@@ -40,6 +40,76 @@ impl LocMapping {
         }
         from
     }
+
+    pub fn sort(&mut self) {
+        self.maps.sort()
+    }
+
+    pub fn convert_range(&self, from: i64, length: i64) -> Vec<i64> {
+        let to = from + length; // To NOT inclusive
+        let mut curr_from = from;
+        let mut result = Vec::<i64>::new();
+        for map in self.maps.iter() {
+            // while long time:
+            if curr_from == to {
+                println!("input finished curr == to");
+                return result;
+            }
+            if curr_from < map.source {
+                result.push(curr_from);
+                if to < map.source {
+                    result.push(to - curr_from);
+                    println!(
+                        "curr < source and to < source => {curr_from} {}",
+                        result.last().unwrap()
+                    );
+                    return result;
+                } else {
+                    result.push(map.source - curr_from);
+                    println!(
+                        "curr < source BUT to > source => limited {curr_from} {}",
+                        result.last().unwrap()
+                    );
+                    curr_from = map.source;
+                }
+            } else {
+                // curr >= map.source
+                if curr_from > map.source + map.length {
+                    println!("Our source is after this mapping, see next mapping");
+                    continue; // Check next map
+                }
+                // Curr is within the mapping
+                result.push(self.convert(curr_from));
+                // Up to where?
+                if to < map.source + map.length {
+                    result.push(to - curr_from);
+                    println!(
+                        "curr >= source and to < end => full inside {curr_from} {}",
+                        result.last().unwrap()
+                    );
+                    curr_from = to;
+                } else {
+                    println!(
+                        "for below: ({} + {length}) - {curr_from} {to} = ...",
+                        map.source
+                    );
+                    result.push((map.source + map.length) - curr_from);
+                    println!(
+                        "curr >= source but to > end; => finish this mapping fully {curr_from} {}",
+                        result.last().unwrap()
+                    );
+                    curr_from = map.source + map.length;
+                }
+            }
+        }
+
+        if curr_from != to {
+            result.push(curr_from);
+            result.push(to - curr_from);
+            println!("Finishing with {curr_from} and {}", result.last().unwrap());
+        }
+        result
+    }
 }
 
 fn solve(input: &[String]) -> (i64, i64) {
@@ -56,11 +126,9 @@ fn solve(input: &[String]) -> (i64, i64) {
             s.parse::<i64>().unwrap()
         })
         .collect();
-    println!("{:?}", input_seeds);
 
     let mut maps = Vec::<LocMapping>::new();
     input.iter().skip(2).for_each(|s| {
-        println!("Handling s: {:?}", s);
         if s.is_empty() {
         } else if s.chars().next().unwrap().is_alphabetic() {
             maps.push(LocMapping::new());
@@ -68,26 +136,52 @@ fn solve(input: &[String]) -> (i64, i64) {
             maps.last_mut().unwrap().maps.push(Map::new(s));
         }
     });
-    // If not mapped => Same number
-    println!("{:?}", maps);
 
+    // If not mapped => Same number
     let output_seeds: Vec<i64> = input_seeds
         .iter()
         .map(|seed| {
             let mut result = *seed;
             for locmap in maps.iter() {
-                println!("curr_seed: {result} for locmap: {:?}", locmap);
                 result = locmap.convert(result);
-                println!("gives: {result}");
             }
             result
         })
         .collect();
 
-    println!("{:?}", output_seeds);
-
     let part1 = *output_seeds.iter().min().unwrap();
-    let part2 = 0;
+
+    // part 2;
+    // Sort all LocMappings to make it more trivial to process
+    maps.iter_mut().for_each(LocMapping::sort);
+    let mut curr_seeds = input_seeds.clone();
+    let mut next_seeds = Vec::<i64>::new();
+
+    // For each locmap, run the
+    maps.iter().for_each(|locmap| {
+        println!("LOCMAP: {:?}", locmap);
+        for seed in curr_seeds.chunks(2) {
+            let new_seeds = locmap.convert_range(*seed.first().unwrap(), *seed.last().unwrap());
+            next_seeds.extend(new_seeds);
+        }
+        println!("NEW_SEED_PAIRS: {:?}", next_seeds);
+
+        curr_seeds = next_seeds.clone();
+        next_seeds = Vec::<i64>::new();
+    });
+    println!("LAST_SEED_PAIRS: {:?}", curr_seeds);
+
+    let mut part2 = i64::MAX;
+    curr_seeds
+        .chunks(2)
+        .map(|pair| {
+            let val = *pair.first().unwrap();
+            if val < part2 {
+                part2 = val;
+            }
+        })
+        .count();
+
     (part1, part2)
 }
 
@@ -121,6 +215,29 @@ mod tests {
 
         let (part1, part2) = solve(&input);
         assert_eq!(part1, 35);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 46);
+    }
+
+    #[test]
+    fn test_locmapping_range() {
+        let mut locmaps = LocMapping::new();
+        locmaps.maps.push(Map {
+            source: 50,
+            destination: 52,
+            length: 48,
+        });
+        locmaps.maps.push(Map {
+            source: 98,
+            destination: 50,
+            length: 2,
+        });
+        let new_seeds = locmaps.convert_range(79, 14);
+        assert_eq!(new_seeds.len(), 2);
+        assert_eq!(new_seeds[0], 81);
+        assert_eq!(new_seeds[1], 14);
+        let new_seeds = locmaps.convert_range(55, 13);
+        assert_eq!(new_seeds.len(), 2);
+        assert_eq!(new_seeds[0], 57);
+        assert_eq!(new_seeds[1], 13);
     }
 }
