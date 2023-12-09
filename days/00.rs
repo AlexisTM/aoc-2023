213d52e265f3e9b177c85daa826a37ebe49a97d3
Alexis Paques <alexis.paques@gmail.com>
2023-12-09 11:37:10 +0100

    Update template

--- days/00.rs
+++ days/00.rs
@@ -4,15 +4,18 @@ fn solve(_input: &[String]) -> (u64, u64) {
     (part1, part2)
 }
 
-fn main() {
-    let input: Vec<String> = std::fs::read_to_string("inputs/00.txt")
+fn read_lines(input_file: &str) -> Vec<String> {
+    std::fs::read_to_string(input_file)
         .unwrap()
         .trim_end()
         .replace("\r\n", "\n")
         .split('\n')
         .map(String::from)
-        .collect();
+        .collect()
+}
 
+fn main() {
+    let input = read_lines("inputs/00.txt");
     let (part1, part2) = solve(&input);
     println!("Part 1: {:?}", part1);
     println!("Part 2: {:?}", part2);
@@ -24,13 +27,7 @@ mod tests {
 
     #[test]
     fn test_data() {
-        let input: Vec<String> = std::fs::read_to_string("inputs/00_test.txt")
-            .unwrap()
-            .trim_end()
-            .replace("\r\n", "\n") // Windows duh
-            .split('\n')
-            .map(String::from)
-            .collect();
+        let input = read_lines("inputs/00_test.txt");
 
         let (part1, part2) = solve(&input);
         assert_eq!(part1, 0);
