283584215a420ae3c6091ead8d85430a08963c2a
Alexis Paques <paa1ti@bosch.com>
2023-12-11 13:15:23 +0100

    Remove overprinting

--- days/10.rs
+++ days/10.rs
@@ -103,7 +103,6 @@ impl Problem {
     }
 
     pub fn navigate(&self, search: &Search, to: Direction) -> Direction {
-        println!("Found: {:?}", search);
         match to {
             Direction::Top => match search.pos.val {
                 Directions::LeftBot => Direction::Left,
@@ -141,7 +140,7 @@ impl Problem {
             Direction::Right => (search.pos.h, search.pos.w + 1),
             Direction::None => return search.clone(),
         };
-        println!("Navigating from {:?} towards {:?}", search, to);
+        // println!("Navigating from {:?} towards {:?}", search, to);
         search.steps += 1;
         search.pos = self.get(next_pose.0, next_pose.1);
 
@@ -155,7 +154,7 @@ impl Problem {
                 Direction::Right => (search.pos.h, search.pos.w + 1),
                 Direction::None => return search.clone(),
             };
-            println!("Navigating from {:?} towards {:?}", search, to);
+            // println!("Navigating from {:?} towards {:?}", search, to);
             search.steps += 1;
             search.pos = self.get(next_pose.0, next_pose.1);
 
@@ -165,36 +164,64 @@ impl Problem {
     }
 
     pub fn navigate_all(&self) -> Vec<Search> {
-        let result = vec![self.go_through(
-            &mut Search {
-                pos: self.start_value,
-                steps: 0,
-            },
-            Direction::Top,
-        ), self.go_through(
-            &mut Search {
-                pos: self.start_value,
-                steps: 0,
-            },
-            Direction::Bot,
-        ), self.go_through(
-            &mut Search {
-                pos: self.start_value,
-                steps: 0,
-            },
-            Direction::Left,
-        ), self.go_through(
-            &mut Search {
-                pos: self.start_value,
-                steps: 0,
-            },
-            Direction::Right,
-        )];
+        let result = vec![
+            self.go_through(
+                &mut Search {
+                    pos: self.start_value,
+                    steps: 0,
+                },
+                Direction::Top,
+            ),
+            self.go_through(
+                &mut Search {
+                    pos: self.start_value,
+                    steps: 0,
+                },
+                Direction::Bot,
+            ),
+            self.go_through(
+                &mut Search {
+                    pos: self.start_value,
+                    steps: 0,
+                },
+                Direction::Left,
+            ),
+            self.go_through(
+                &mut Search {
+                    pos: self.start_value,
+                    steps: 0,
+                },
+                Direction::Right,
+            ),
+        ];
         result
     }
 }
 
 fn solve_part1(input: &[String]) -> u64 {
+    let input = input
+        .iter()
+        .map(|s| s.chars().map(|c| c.into()).collect_vec())
+        .collect_vec();
+
+    let problem = Problem::new(input);
+
+    let results = problem.navigate_all();
+    for s in results.iter() {
+        println!("Results: {:?}", s);
+    }
+    println!();
+    let result = results
+        .iter()
+        .filter(|s| s.pos.val == Directions::Starting)
+        .max_by(|s1, s2| s1.steps.cmp(&s2.steps))
+        .unwrap();
+    println!("Results: {:?}", result);
+
+    result.steps as u64 / 2u64 + (result.steps as u64 & 1u64)
+}
+
+fn solve_part2(input: &[String]) -> u64 {
     let input = input
         .iter()
         .map(|s| s.chars().map(|c| c.into()).collect_vec())
@@ -271,5 +298,50 @@ LJ..."
 
         let part1 = solve_part1(&input);
         assert_eq!(part1, 8);
+    }
+
+    #[test]
+    fn test_part2_simple() {
+        let input: Vec<_> = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."
+            .to_owned()
+            .trim_end()
+            .replace("\r\n", "\n")
+            .split('\n')
+            .map(String::from)
+            .collect();
+
+        let part2 = solve_part2(&input);
+        assert_eq!(part2, 4);
+    }
+
+    #[test]
+    fn test_part2_complex() {
+        let input: Vec<_> = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."
+            .to_owned()
+            .trim_end()
+            .replace("\r\n", "\n")
+            .split('\n')
+            .map(String::from)
+            .collect();
+
+        let part2 = solve_part2(&input);
+        assert_eq!(part2, 8);
     }
 }
