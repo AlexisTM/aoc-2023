b1b3796c3e3c1afad75c996810b51068343f0d44
Alexis Paques <paa1ti@bosch.com>
2023-12-13 13:47:17 +0100

    Day 13p1

--- days/13.rs
+++ days/13.rs
@@ -1,5 +1,69 @@
-fn solve(_input: &[String]) -> (u64, u64) {
-    let part1 = 0;
+use itertools::Itertools;
+
+fn is_mirrored(mirror: i64, game: &[Vec<char>]) -> bool {
+    for checked_value in 0..(mirror + 1) {
+        let left_index = mirror - (checked_value);
+        let right_index = mirror + (checked_value + 1);
+
+        if left_index < 0 || right_index >= game.len() as i64 {
+            break;
+        }
+
+        // In preparation for part2
+        let diff = game[left_index as usize]
+            .iter()
+            .zip(&game[right_index as usize])
+            .fold(0, |acc, (a, b)| if a != b { acc + 1 } else { acc });
+
+        if diff > 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn find_mirrors(game: &[Vec<char>]) -> (Vec<u64>, Vec<u64>) {
+    let mut row_mirrors = Vec::<u64>::new();
+    let mut col_mirrors = Vec::<u64>::new();
+
+    let row_count = game.len();
+    for mirror in 0..(row_count - 1) {
+        if is_mirrored(mirror as i64, game) {
+            row_mirrors.push((mirror + 1) as u64);
+        }
+    }
+    let transposed_game: Vec<Vec<char>> = (0..game[0].len())
+        .map(|col| game.iter().map(|row| row[col]).collect())
+        .collect();
+
+    let col_count = transposed_game.len();
+    for mirror in 0..(col_count - 1) {
+        if is_mirrored(mirror as i64, &transposed_game) {
+            col_mirrors.push((mirror + 1) as u64);
+        }
+    }
+
+    (row_mirrors, col_mirrors)
+}
+
+fn solve(input: &[String]) -> (u64, u64) {
+    let mut games = Vec::<Vec<Vec<char>>>::new();
+    games.push(Vec::<Vec<char>>::new());
+    input.iter().for_each(|f| {
+        if f.is_empty() {
+            games.push(Vec::<Vec<char>>::new());
+        } else {
+            games.last_mut().unwrap().push(f.chars().collect_vec());
+        }
+    });
+
+    let mut part1 = 0;
+    for game in games.iter() {
+        let (row_mirrors, col_mirrors) = find_mirrors(game);
+        part1 +=
+            col_mirrors.iter().sum::<u64>() + row_mirrors.iter().fold(0u64, |acc, v| acc + v * 100);
+    }
+
     let part2 = 0;
     (part1, part2)
 }
@@ -30,7 +94,7 @@ mod tests {
         let input = read_lines("inputs/13_test.txt");
 
         let (part1, part2) = solve(&input);
-        assert_eq!(part1, 0);
+        assert_eq!(part1, 405);
         assert_eq!(part2, 0);
     }
 }
