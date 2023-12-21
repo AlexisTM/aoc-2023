4266cab4cf7db426232afdf2e5421d0be0e28b30
Alexis Paques <alexis.paques@gmail.com>
2023-12-21 13:51:40 +0100

    day 18 & 19 boilerplate

---
+++ days/18.rs
@@ -0,0 +1,36 @@
+fn solve(_input: &[String]) -> (u64, u64) {
+    let part1 = 0;
+    let part2 = 0;
+    (part1, part2)
+}
+
+fn read_lines(input_file: &str) -> Vec<String> {
+    std::fs::read_to_string(input_file)
+        .unwrap()
+        .trim_end()
+        .replace("\r\n", "\n")
+        .split('\n')
+        .map(String::from)
+        .collect()
+}
+
+fn main() {
+    let input = read_lines("inputs/18.txt");
+    let (part1, part2) = solve(&input);
+    println!("Part 1: {:?}", part1);
+    println!("Part 2: {:?}", part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_data() {
+        let input = read_lines("inputs/18_test.txt");
+
+        let (part1, part2) = solve(&input);
+        assert_eq!(part1, 0);
+        assert_eq!(part2, 0);
+    }
+}
